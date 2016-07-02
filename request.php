40c758e33709455c0196a0a7eae8bcfcbb5d341b
aqu_qlc <mvnquang@gmail.com>
2016-07-02 12:19:22 +0700

    no message

--- request.php
+++ request.php
@@ -1,5 +1,5 @@
 <?php
-require('Services/Twilio/Capability.php');
+require('Services/Twilio.php');
 header('Content-Type: text/xml');
 $accountSid = getenv('TWILIO_ACCOUNT_SID');
 $authToken = getenv('TWILIO_AUTH_TOKEN');
@@ -10,37 +10,35 @@ $appSid = getenv('TWILIO_APP_SID');
 
 $requestFunction = $_REQUEST['requestFunction']?:'';
 $numberPhone = $_REQUEST['numberPhone']?:'';
-$client = new Services_Twilio($accountSid, $authToken);
+class RequestObj{
+	  public function __construct() {
+	  
+    	}
+    
+    public function ValidateNumber(){
+	   $client = new Services_Twilio($accountSid, $authToken);
  		$response = $client->account->outgoing_caller_ids->create($numberPhone);
-//  		echo json_encode(array('numberPhone'=>$numberPhone, 'code'=>$response));
- 		
-		// $client = new Services_Twilio('AC123', '123');
-// 		foreach ($client->account->outgoing_caller_ids as $caller_id) {
-// 		  print $caller_id->friendly_name;
-// 		}
-	echo json_encode(array('numberPhone'=>$numberPhone, 'code'=>'12357'));
-// class RequestValidatorTest{
-// 	  public function __construct() {
-// 	   $this->ValidateNumbar();
-//        		if($requestFunction == 'requestValid'){
-// // 				$this->ValidateNumbar();
-// 				echo 'valid phonenumber';
-//        		} else {
-//        			echo 'not valid';
-//        		}
-//     	}
-//     
-//     public function ValidateNumbar(){
-// 	   $client = new Services_Twilio($accountSid, $authToken);
-//  		$response = $client->account->outgoing_caller_ids->create($numberPhone);
-// //  		echo json_encode(array('numberPhone'=>$numberPhone, 'code'=>$response));
-//  		
-// 		// $client = new Services_Twilio('AC123', '123');
-// // 		foreach ($client->account->outgoing_caller_ids as $caller_id) {
-// // 		  print $caller_id->friendly_name;
-// // 		}
-// 	echo json_encode(array('numberPhone'=>$numberPhone, 'code'=>'12357'));
-//     }
-// }
 
+		echo json_encode(array('numberPhone'=>$numberPhone, 'code'=>'12357'));
+    }
+    
+    public function getNumberValidated(){	
+		$client = new Services_Twilio('AC123', '123');
+		$arrayObject = array('numberPhone'=>$numberPhone, 'code'=>'12357');
+		foreach ($client->account->outgoing_caller_ids as $caller_id) {
+			$arrayObject[] = $caller_id->friendly_name;
+			
+		echo json_encode($arrayObject);
+    }
+
+}
+
+$object = new RequestObj();
+	if($requestFunction == 'requestValid'){
+		$object->ValidateNumber();
+		echo 'valid phonenumber';
+	} else if($requestFunction = 'requestValidList'){
+		$object->getNumberValidated();
+		echo 'not valid';
+	}
 ?>
