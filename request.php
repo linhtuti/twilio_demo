9b6d25bc26050c7fed3d9b1f6680792b7bd4fc86
aqu_qlc <mvnquang@gmail.com>
2016-07-02 11:29:03 +0700

    no message

---
+++ request.php
@@ -0,0 +1,31 @@
+<?php
+require('Services/Twilio/Capability.php');
+header('Content-Type: text/xml');
+$accountSid = getenv('TWILIO_ACCOUNT_SID');
+$authToken = getenv('TWILIO_AUTH_TOKEN');
+
+$appSid = getenv('TWILIO_APP_SID');
+
+
+
+$requestFunction = $_REQUEST['requestFunction']?:'';
+$numberPhone = $_REQUEST['numberPhone'] ?: '';
+class RequestValidatorTest{
+	  public function __construct() {
+       		if($requestFunction == ''){
+				$this->ValidateNumbar($url, $data),
+				echo 'valid phonenumber';
+       		} else {
+       			echo 'not valid';
+       		}
+    	}
+    
+    public function ValidateNumbar(){
+	   $client = new Services_Twilio($accountSid, $authToken);
+ 		$response = $client->account->outgoing_caller_ids->create($numberPhone);
+ 		echo json_encode(array('numberPhone'=>$numberPhone, 'code'=>$response));
+// 	echo json_encode(array('numberPhone'=>$numberPhone, 'code'=>'12357'));
+    }
+}
+
+?>
---
+++ twiml.php
@@ -0,0 +1,45 @@
+<?php
+require 'vendor/autoload.php';
+header('Content-Type: text/xml');
+
+$from     = $_REQUEST['From'];
+$callee   = $_REQUEST['callee'];
+$callerId = $_REQUEST['callerId'];
+$digits   = $_REQUEST['Digits'];
+$response = new Services_Twilio_Twiml();
+
+# user chose their callee using <Gather>
+if (isset($digits) && !$callee) {
+    $callee = $_REQUEST[$digits];
+}
+
+#call 
+if ($callee) {
+    $response->dial($callee, array('callerId'=>$callerId));
+} else if (preg_match("/^[\d\(\)\-\+ ]+$/", $from)) {
+    $contents = @file_get_contents('clients.txt');
+    $lines = array_slice(array_filter(explode(',', $contents)), 0, 10);
+    if (!$contents) {
+        $response->dial('default', array('callerId'=>$callerId));
+    } else if (count($lines) == 1) {
+        $response->dial($lines[0], array('callerId'=>$callerId));
+    } else {
+        # Since we don't know (from the Request params) whom a
+        # regular PSTN phone wants to call, let them choose
+        $options = $lines;
+        array_walk($options, function(&$elt, $idx){ $elt = "$idx to call $elt"; });
+        $say = 'Press ' . implode(', ', $options) . '.';
+        $here = pathinfo(__FILE__, PATHINFO_BASENAME);
+        $gather = $response->gather(array('action'=>"/{$here}?".http_build_query($lines),
+                                          'numDigits'=>'1'));
+        $gather->say($say, array('voice'=>'woman'));
+    }
+} else {
+    $response->say('Congratulations! You just made a call using Twilio Client! That is awesome!', 
+                   array('voice'=>'woman'));
+}
+
+#add callid
+
+print $response;
+?>
